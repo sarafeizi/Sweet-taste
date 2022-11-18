1fc6c47c622bd689f5a90a8ab5f99b33bb998ae6
sara.fz <sarafz9978@gmail.com>
2022-11-18 14:05:55 +0330

    new

--- src/components/Error.js
+++ src/components/Error.js
@@ -10,10 +10,10 @@ const Error = () => {
         <div className={styles.bigeror}>
  
         <div  className={styles.container}  >
-            <div  className={ styles.fourleft}><h1>4</h1></div>
+            <div  className={ styles.fourleft}><h1 className={styles.largeSizeH}>4</h1></div>
             <div  className={styles.pizza}  src={eror}>  </div>
             <div  className={styles.fourright}><h1  className={styles.largeSizeH}>4</h1></div>
-            <p  className={styles.text}>Sorry. The page not found !!</p>
+            <p  className={styles.text}>متاسفم مثل اینکه راه رو اشتباه اومدی :(</p>
         </div>
          
          
