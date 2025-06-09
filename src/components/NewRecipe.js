530295bd940f6473000051229e51c8d847d69e8b
SaraFeizi <sarafz9978@gmail.com>
2025-06-09 12:17:14 +0330

    new

--- src/components/NewRecipe.js
+++ src/components/NewRecipe.js
@@ -6,90 +6,68 @@ import panjahse from "../image/ghorme-sabzi.jpg";
 import panjahdo from "../image/85973qc2g.jpg";
 import Slider from 'react-slick';
 
+const recipes = [
+  { id: 1, img: panjahdo, title: "پای سیب" },
+  { id: 2, img: panjah, title: "ماکارونی" },
+  { id: 3, img: panjahyek, title: "سوشی" },
+  { id: 4, img: panjahse, title: "قرمه سبزی" },
+];
+
 const NewRecipe = () => {
-     const settings_3 = {
-          dots: false,
-          infinite: true,
-          slidesToShow: 3,
+  const settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    rtl: true,
+    responsive: [
+      {
+        breakpoint: 860,
+        settings: {
+          slidesToShow: 1,
           slidesToScroll: 1,
-          autoplay: true,
-          autoplaySpeed: 2000,
-          rtl: true,
-          responsive: [{
-               breakpoint: 860,
-               settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    initialSlide: 2,
-               }
-          }]
-     }
+          initialSlide: 2,
+        },
+      },
+    ],
+  };
 
-     return (
-          <div className={styles.all}>
-               <div className={styles.bignew3}>
-                    <div className={styles.bignew}>
-                         <div className={`${styles.bignew2} row `}>
-                              <nav className={`${styles.lastnav} container `} >
+  return (
+    <div className={styles.all}>
+      <div className={styles.bignew3}>
+        <div className={styles.bignew}>
+          <div className={`${styles.bignew2} row`}>
+            <nav className={`${styles.lastnav} container`}>
+              <h3 className={styles.h3nav}>
+                پیشنهادات شما که در آینده داخل سایت قرار میگیرد
+              </h3>
+            </nav>
+            <div className={`${styles.bordernew} col-12 mx-auto col-md-6`}></div>
 
-                                   <h3 className={styles.h3nav}> پیشنهادات شما که در آینده داخل سایت قرار میگیرد</h3>
-                              </nav>
-                              <div className={`${styles.bordernew} col-12 mx-auto co-md-6`}></div>
-                              <div className={`${styles.nav1} row `}>
-                                   <Slider    {...settings_3}>
-                                        <div>
-                                             <div className={`${styles.nav}   `}>
-                                                  <div className={styles.bigimgnav}>
-                                                       <img src={panjahdo} className={styles.imgnav} alt="panjah" />
-                                                  </div>
-                                                  <br />
-                                                  <p className={styles.anav}>
-                                                       پای سیب
-                                                  </p>
-                                             </div>
-                                        </div>
-                                        <div>
-                                             <div className={`${styles.nav}  `}>
-                                                  <div className={styles.bigimgnav}>
-                                                       <img src={panjah} className={styles.imgnav} alt="panjah" />
-                                                  </div>
-                                                  <br />
-                                                  <p href="#" className={styles.anav}>
-                                                       ماکارونی
-                                                  </p>
-                                             </div>
-                                        </div>
-                                        <div>
-                                             <div className={`${styles.nav}   `}>
-                                                  <div className={styles.bigimgnav}>
-                                                       <img src={panjahyek} className={styles.imgnav} alt="panjah" />
-                                                  </div>
-                                                  <br />
-                                                  <p href="#" className={styles.anav}>
-                                                       سوشی
-                                                  </p>
-                                             </div>
-                                        </div>
-                                        <div>
-                                             <div className={`${styles.nav}  `}>
-                                                  <div className={styles.bigimgnav}>
-                                                       <img src={panjahse} className={styles.imgnav} alt="panjah" />
-                                                  </div>
-                                                  <br />
-                                                  <p href="#" className={styles.anav}>
-                                                       قرمه سبزی
-                                                  </p>
-                                             </div>
-                                        </div>
-                                   </Slider>
-                                   <div className={`${styles.bordernew1} col-12 mx-auto co-md-6`}></div>
-                              </div>
-                         </div>
+            <div className={`${styles.nav1} row`}>
+              <Slider {...settings}>
+                {recipes.map(({ id, img, title }) => (
+                  <div key={id}>
+                    <div className={styles.nav}>
+                      <div className={styles.bigimgnav}>
+                        <img src={img} alt={title} className={styles.imgnav} />
+                      </div>
+                      <br />
+                      <p className={styles.anav}>{title}</p>
                     </div>
-               </div>
-          </div>
-     );
-}
+                  </div>
+                ))}
+              </Slider>
 
+              <div className={`${styles.bordernew1} col-12 mx-auto col-md-6`}></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default NewRecipe;
