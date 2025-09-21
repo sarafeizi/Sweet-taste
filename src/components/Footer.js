12f973d0c46e15cfc0b1f6c35effd3e7cbcf9ae9
SaraFeizi <sarafz9978@gmail.com>
2025-09-21 15:57:35 +0330

    new

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,10 +11,17 @@ const Footer = () => {
             <div className={styles.backnav}>
                 <p className={styles.backnavp}>مارا در شبکه های اجتماعی دنبال کنید</p>
                 <div className={styles.footericon}>
-                    <Link to="https://t.me/sarafz99"><img src={telegram} alt="Telegram" className={styles.logoicon} /></Link>
-                    <Link to="https://www.instagram.com/sarafz99"><img src={instagram} alt="Instagram" className={styles.logoicon} /></Link>
-                    <Link to="https://teams.microsoft.com/l/chat/0/0?users=sarafz9978@gmail.com"><img src={google} alt="Google" className={styles.logoicon} /></Link>
+                    <a href="https://t.me/sarafz99" target="_blank" rel="noopener noreferrer">
+                        <img src={telegram} alt="Telegram" className={styles.logoicon} />
+                    </a>
+                    <a href="https://www.instagram.com/sarafz99" target="_blank" rel="noopener noreferrer">
+                        <img src={instagram} alt="Instagram" className={styles.logoicon} />
+                    </a>
+                    <a href="https://teams.microsoft.com/l/chat/0/0?users=sarafz9978@gmail.com" target="_blank" rel="noopener noreferrer">
+                        <img src={google} alt="Google" className={styles.logoicon} />
+                    </a>
                 </div>
+
             </div>
             <div className={styles.copyright}>
                 <p className={styles.pcopyright}>© Copyright Of This Site Belongs To Its Owner :D</p>
